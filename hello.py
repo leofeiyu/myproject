2e6b13a6971a2e010bfcbede1a7da1c585356ebc
David Zhang <641206446@qq.com>
2017-07-12 17:12:06 +0800

    add email support

--- hello.py
+++ hello.py
@@ -3,23 +3,52 @@
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
 from flask_moment import Moment
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 import os
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# 配置Flask-Mail使用QQ邮箱
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+# 从环境中导入敏感信息
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASK_ADMIN'] = os.environ.get('FLASK_ADMIN')
+# 在程序中集成发送电子邮件的功能
+app.config['FLASK_MAIL_SUBJECT_PREFIX'] = "[J's Book]"
+app.config['FLASK_MAIL_SENDER'] = os.environ.get('FLASK_MAIL_SENDER')
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASK_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASK_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 manager = Manager(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+manager.add_command('db', MigrateCommand)
 moment = Moment(app)
+
 
 # 为shell命令添加一个上下文，使其自动导入特定的对象
 def make_shell_context():
@@ -61,6 +90,9 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASK_ADMIN']:
+                send_mail(app.config['FLASK_ADMIN'], 'New User',
+                          'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
